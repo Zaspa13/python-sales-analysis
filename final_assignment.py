6c6eba58a56ca3b4b10d8df9baa0371c76d4551e
Zaspa13 <zpasp13@gmail.com>
2026-02-17 21:40:48 +0200

    Refactor final_assignment.py for data analysis tasks

--- final_assignment.py
+++ final_assignment.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 import plotly.express as px
 
-df = pd.read_csv('finance_liquor_sales.csv')
+df = pd.read_csv('data/finance_liquor_sales.csv')
 
 #Καθαρισμός Dataset
 print(df.dtypes)
@@ -31,4 +31,5 @@ fig=px.bar(top_sales, x='percent', y='store_name', orientation='h', color='perce
                labels={'percent': 'Sales Share (%)', 'store_name': 'Store Name'})
 fig.update_layout(yaxis={'categoryorder':'total ascending'})
 fig.update_traces(texttemplate='%{x:.2f}%', textposition='outside')
+
 fig.show()
